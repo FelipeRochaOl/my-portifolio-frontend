814e5d051a974ff58f292e133449e4be85a35214
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-01-07 01:41:23 -0300

    Receive challenge

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
 
-import "./styles.css";
+import './styles.css'
 
 function App() {
+  const [repository, setRepository] = useState([])
+  const [repositoryTitle, setRepositoryTitle] = useState('')
+
+  useEffect(() => {
+    getRepositories()
+  }, [])
+
+  async function getRepositories() {
+    const response = await api.get('/repositories')
+    setRepository(response.data)
+  }
+
+  async function handleRepositoryTitle(event) {
+    setRepositoryTitle(event.target.value)
+  }
+
   async function handleAddRepository() {
-    // TODO
+    const newRepository = await api.post('/repositories', {title:`${repositoryTitle}`, url: 'url', 'techs': ['teste']})
+    setRepository([...repository, newRepository.data])
+    setRepositoryTitle('')
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`)
+    //getRepositories() pra mim esta seria a forma correta
+    setRepository(repository.filter(elem => elem.id !== id))
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
+      <label>Title</label>
+      <input type="text" onChange={handleRepositoryTitle} value={repositoryTitle}/>
       <button onClick={handleAddRepository}>Adicionar</button>
+      <ul data-testid='repository-list'>
+        {repository.map(data => {
+          return (
+            <li key={data.id}>
+              {data.title}
+              <button onClick={() => handleRemoveRepository(data.id)}>
+                Remover
+              </button>         
+            </li>
+          )
+        })}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
